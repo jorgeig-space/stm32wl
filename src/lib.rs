465e974e0f3c077f3e2c7d7e8a7dbe7e335127fe
Jorgeig <jorge@jorgeig.com>
2021-08-06 14:58:14 +0800

    Update to latest stm32-rs branch

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! Peripheral access API for STM32WL microcontrollers
 //! (generated using [svd2rust](https://github.com/rust-embedded/svd2rust)
-//! 0.17.0)
+//! 0.19.0)
 //!
 //! You can find an overview of the API here:
-//! [svd2rust/#peripheral-api](https://docs.rs/svd2rust/0.17.0/svd2rust/#peripheral-api)
+//! [svd2rust/#peripheral-api](https://docs.rs/svd2rust/0.19.0/svd2rust/#peripheral-api)
 //!
 //! For more details see the README here:
 //! [stm32-rs](https://github.com/stm32-rs/stm32-rs)
@@ -17,6 +17,7 @@
 //! available registers and fields see: [stm32-rs Device Coverage](https://stm32-rs.github.io/stm32-rs/)
 
 #![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 #![no_std]
 
 mod generic;
